ec3f80d41b01ae11f702dede16e088780b5772b2
Cheng Gong <enigne@gmail.com>
2022-07-25 06:16:24 -0400

    relaxation dx=5km

--- myexperiments.m
+++ myexperiments.m
@@ -3,7 +3,7 @@ close all
 
 today = datestr(date(), 'yyyymmdd');
 
-experiments = [4];
+experiments = [5];
 flowmodel = 'MOLHO';
 
 if any(experiments == 1) % exp 1: spin up on a coarse mesh dx=20km {{{
