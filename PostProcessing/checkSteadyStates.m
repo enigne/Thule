2d51be22a902ca70ad9f24721fe4f86212aafb35
Cheng Gong <enigne@gmail.com>
2022-08-03 08:59:20 -0400

    check convergence of steady states

--- PostProcessing/checkSteadyStates.m
+++ PostProcessing/checkSteadyStates.m
@@ -1,13 +1,14 @@
 clear
 close all
-glacier = 'Thule';
-projPath = ['/Users/gongcheng/Dartmouth/', glacier];
+addpath('../')
+projectsettings;
+
 resolutions = [2e3, 5e3, 10e3, 20e3];
 for i = 1:length(resolutions)
-    [time, normDvel] = convergenceSteadyState(resolutions(i));
+    [time, normDvel] = convergenceSteadyState(resolutions(i), glacier);
     semilogy(time(2:end), normDvel,'o')
     hold on
 end
 legend({'2km', '5km', '10km', '20km'})
 xlabel('t (years)')
-ylabel('max(diff(vel))')
+ylabel('max(diff(vel))/diff(time)')
