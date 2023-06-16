e25199ff0e37e3bf71701090fbb4975d99540c52
Cheng Gong <enigne@gmail.com>
2023-06-16 11:27:47 -0400

    rerun T=1e4, 5km res EXP3

--- myexperiments.m
+++ myexperiments.m
@@ -4,7 +4,7 @@ close all
 
 today = datestr(date(), 'yyyymmdd');
 
-experiments = [3];
+experiments = [4];
 flowmodel = 'SSA';
 
 if any(experiments == 1) % exp 1: spin up on a coarse mesh dx=10km {{{
