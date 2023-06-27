3513f996228b33b812d61c2ed1c6085638b883bc
Cheng Gong <enigne@gmail.com>
2023-06-27 14:00:43 -0400

    change projection method to all time steps

--- PostProcessing/project2Profile.m
+++ PostProcessing/project2Profile.m
@@ -10,8 +10,8 @@ function pf = project2Profile(md, pf)
 	x = md.mesh.x;
 	y = md.mesh.y;
 	pf.bed = InterpFromMeshToMesh2d(index, x, y, md.geometry.bed, px, py);
-	pf.base = InterpFromMeshToMesh2d(index, x, y, md.results.TransientSolution(end).Base, px, py);
-	pf.thickness = InterpFromMeshToMesh2d(index, x, y, md.results.TransientSolution(end).Thickness, px, py);
-	pf.vx = InterpFromMeshToMesh2d(index, x, y, md.results.TransientSolution(end).Vx, px, py);
-	pf.vy = InterpFromMeshToMesh2d(index, x, y, md.results.TransientSolution(end).Vy, px, py);
+	pf.base = InterpFromMeshToMesh2d(index, x, y, [md.results.TransientSolution(:).Base], px, py);
+	pf.thickness = InterpFromMeshToMesh2d(index, x, y, [md.results.TransientSolution(:).Thickness], px, py);
+	pf.vx = InterpFromMeshToMesh2d(index, x, y, [md.results.TransientSolution(:).Vx], px, py);
+	pf.vy = InterpFromMeshToMesh2d(index, x, y, [md.results.TransientSolution(:).Vy], px, py);
 end
