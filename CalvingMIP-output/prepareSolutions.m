5a26cf6287ee17f620cbb5b90028a9f3fb66cc29
Cheng Gong <enigne@gmail.com>
2024-07-18 16:00:54 -0400

    prepare solutions

--- CalvingMIP-output/prepareSolutions.m
+++ CalvingMIP-output/prepareSolutions.m
@@ -10,18 +10,32 @@ projectsettings;
 glacier = 'Thule';
 saveflag = 1;
 projPath = ['/totten_1/chenggong/', glacier, '/'];
-filename = 'CalvingMIP_Circ_5km_HO_Exp2_save2d'; frontdata = 'EXP2_HO';
+EXP = 'EXP4';
+%filename = 'CalvingMIP_Circ_5km_HO_Exp2_save2d'; frontdata = 'EXP2_HO';
 %filename = 'cmip-AWI-ex2-G5000-refined-pulse-linear-normal-100_save2d'; frontdata = 'EXP2_AWI';
 %filename = 'cmip-AWI-ex4-G5000-refined-pulse-linear-normal-100_save2d'; frontdata = 'EXP4_AWI';
+filename = '../Models/20230709_EXP4_res_5000/Model_Thule_Transient'; frontdata = 'EXP4_Dartmouth';
 
 % Loading data 
 org=organizer('repository', [projPath, 'CalvingMIP-output/'], 'prefix', '', 'steps', 0);
 disp(['Load the model from ', filename])
 md = loadmodel(org, filename);
 % get the solutions along the profiles
-suffixname = 'Circle';
-P = readtable([projPath, 'PostProcessing/Results/', suffixname, '_Profiles.csv']);
-profiles = project2Profiles(md, P, suffixname);
+
+if strcmp(EXP, 'EXP2')
+	suffixname = 'Circle';
+	P = readtable([projPath, 'PostProcessing/Results/', suffixname, '_Profiles.csv']);
+	profiles = project2Profiles(md, P, suffixname);
+elseif strcmp(EXP, 'EXP4')
+	suffixname = 'Caprona';
+	P = readtable([projPath, 'PostProcessing/Results/', suffixname, '_Profiles.csv']);
+	profiles = project2Profiles(md, P, suffixname);
+	suffixname = 'Halbrane';
+	Q = readtable([projPath, 'PostProcessing/Results/', suffixname, '_Profiles.csv']);
+	Q_profiles = project2Profiles(md, Q, suffixname);
+	% merge Q_profiles to profiles
+	profiles = mergeProfiles(profiles, Q_profiles);
+end
 
 % process
 names = fieldnames(profiles);
